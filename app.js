0feca74e9d6d446d30bd45e7b2c5512414e4f7fe
galiphasandugan <galiphasandugan@gmail.com>
2022-06-14 00:05:03 +0300

    finally

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const buton =document.getElementById("btn");
-const b = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-console.log(b);
-buton.addEventListener("click", (e)=> {
-const input =document.querySelector(".input");
-console.log(input.value);
+const buton = document.querySelector(".btn");
+const random = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+const input = document.querySelector(".input");
+const sonuc = document.querySelector(".sonuc");
 
-  if (input.value < b) {
-    alert('artır');
-    e.target
-  };  
-  if (input.value > b) {
-   alert('azalt');
-   e.target
-  };  
-  if (input.value == b) {
-   alert('buldun');
-   
-};
-   
+buton.addEventListener("click", () => {
+  console.log(input.value.type);
+
+  if (input.value > random) {
+    sonuc.value = "sayıyı azalt";
+  }
+  if (input.value < random) {
+    sonuc.value = "sayıyı artır";
+  }
+  if (input.value == random) {
+    sonuc.value = "KAZANDIN";
+  }
+  input.value = "";
+  input.focus();
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    buton.click();
+  }
+});
+
+window.addEventListener("load", (e) => {
+  input.focus();
 });
